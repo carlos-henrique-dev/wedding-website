6a9115280d84947b358c1f1db8cfbe28e8af757c
Carlos Henrique <henriqueok20@gmail.com>
2023-04-29 23:50:32 -0400

    fix metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const OG_IMAGE = 'https://drive.google.com/file/d/1RoG_1XOZj1gdOH5glt58FWQFJcj5cYha/view?usp=share_link'
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,11 +11,11 @@ export default function Document() {
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://henriquethaysa-wedding.com/" />
-		<meta property="og:image" content="https://drive.google.com/file/d/1fxpTu0pw0nTGX_5tsQkIOOja8evBKHcW/view?usp=share_link" />
-		<meta property="og:image:secure_url" content="https://drive.google.com/file/d/1fxpTu0pw0nTGX_5tsQkIOOja8evBKHcW/view?usp=share_link" /> 
-		<meta property="og:image:type" content="image/jpeg" /> 
-		<meta property="og:image:width" content="300" /> 
-		<meta property="og:image:height" content="300" />
+        <meta property="og:image" content={OG_IMAGE} />
+        <meta property="og:image:secure_url" content={OG_IMAGE} />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
       </Head>
 
       <body>
