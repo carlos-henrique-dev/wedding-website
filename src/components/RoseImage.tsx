0249e4db31853bae0405bde4874145b2f9b74489
Carlos Henrique <henriqueok20@gmail.com>
2023-04-29 22:47:27 -0400

    fix homepage layout

--- src/components/RoseImage.tsx
+++ src/components/RoseImage.tsx
@@ -1,3 +1,7 @@
 import Image from 'next/image'
 
-export const RoseImage = () => <Image src="/images/rose.png" alt="background-rose" width="860" height="910" />
+interface Props {
+  className?: string
+}
+
+export const RoseImage = ({ className }: Props) => <Image src="/images/rose.png" alt="background-rose" width="860" height="910" className={className} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,30 +9,29 @@ export default function Home() {
       </Head>
 
       <main className="home">
-        <div className="home-img-top">
-          <RoseImage />
+        <RoseImage className="home-img-top" />
+        <RoseImage className="home-img-bottom" />
+
+        <div className="home-content">
+          <h1 className="home-title">
+            <span>Henrique</span>
+            <span>e</span>
+            <span>Thaysa</span>
+          </h1>
+
+          <p className="home-subtitle">
+            <div className="home-subtitle-main">
+              <span>&quot;Grandes coisas fez o Senhor por nós.</span>
+              <span>Por isso estamos alegres.&quot;</span>
+            </div>
+
+            <span>Salmos 126:3</span>
+          </p>
+
+          <p className="home-date">
+            <span>15 - 07 - 2023</span>
+          </p>
         </div>
-
-        <div className="home-img-bottom">
-          <RoseImage />
-        </div>
-
-        <h1 className="home-title">
-			<span>Henrique</span>
-			<span> e </span>
-			<span>Thaysa</span>
-		</h1>
-
-        <p className="home-subtitle">
-          <span>&quot;Grandes coisas fez o Senhor por nós. </span>
-		  <span>Por isso estamos alegres.&quot;</span>
-          <br />
-          <span>Salmos 126:3</span>
-        </p>
-
-        <p className="home-date">
-          <span>15 - 07 - 2023</span>
-        </p>
       </main>
     </>
   )
