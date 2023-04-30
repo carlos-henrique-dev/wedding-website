0a24aa8d2ec050fa8b2cc7a756730a8139149029
Carlos Henrique <henriqueok20@gmail.com>
2023-04-29 21:36:23 -0400

    add environment variable

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,11 +2,11 @@ import { EnvironmentVariables } from '@/interfaces'
 
 const { FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENTE_MAIL, API_URL } = process.env
 
-const privateKey = JSON.parse(FIREBASE_PRIVATE_KEY || '{}').privateKey
+// const privateKey = JSON.parse(FIREBASE_PRIVATE_KEY || '{}').privateKey
 
 export const environment: EnvironmentVariables = {
   projectId: FIREBASE_PROJECT_ID as string,
-  privateKey,
+  privateKey: FIREBASE_PRIVATE_KEY as string,
   clientEmail: FIREBASE_CLIENTE_MAIL as string,
   apiUrl: API_URL as string,
 }
